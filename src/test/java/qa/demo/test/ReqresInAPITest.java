d39b802653edc4561690fcbc2ac26cd504e65c99
Alexsandra-Vedenyapina <sahynia@mail.ru>
2026-09-06 10:08:41 +0300

    Дополнен тест-класс ReqresInAPITest

--- src/test/java/qa/demo/test/ReqresInAPITest.java
+++ src/test/java/qa/demo/test/ReqresInAPITest.java
@@ -1,17 +1,76 @@
 package qa.demo.test;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CRUDUserAPITest {
+public class ReqresInAPITest {
     private String nameUser="Tratila";
     private String jobUser="Cat";
     private String idUSer;
     private String jobNew="Super Cat";
     private String url="https://reqres.in/api/users";
+
+    @Test
+    void testGetUsersReturnStatusCode200(){
+        given()
+                .log().all()
+                .when()
+                .get(url+"?page=1")
+                .then()
+                .log().status()
+                .log().body();
+
+    }
+
+    @Test
+    void testGetUsersReturnValidJsonStructure(){
+        given()
+                .log().all()
+                .when()
+                .get(url+"?page=1")
+                .then()
+                .log().status()
+                .log().body()
+                .extract().path("page", "per_page","total","total_pages", "data");
+
+    }
+
+    @Test
+    void testDataSizeEquelsPerPageValue(){
+        var response = given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .when()
+                .get(url+"?page=1")
+                .then()
+                .log().status()
+                .log().body()
+                .extract().response();
+
+        int perPage = response.path("per_page");
+        int dataSize = response.path("data.size()");
+
+        assertEquals(perPage,dataSize, "Количество записей в data("+dataSize+") должно быть равно per_page("+perPage);
+
+    }
+
+    @Test
+    void testSuccessGetUser(){
+        given()
+                .log().uri()
+                .when()
+                .get(url+"/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(2));
+    }
 
     @Test
     void testSuccessUserCreation(){
@@ -72,4 +131,18 @@ public class CRUDUserAPITest {
                 .log().body()
                 .statusCode(404);
     }
+
+    @Test
+    void testSuccessUserDeleted(){
+        given()
+                .log().uri()
+                .when()
+                .delete(url+"/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+
+    }
 }
+
